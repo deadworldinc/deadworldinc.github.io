f3d9280f2823778e103243154e14ffb9d2c2272d
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-03 21:38:48 +0700

    Create browsercheck.js

---
+++ browsercheck.js
@@ -0,0 +1,4 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+
+    document.getElementById("txtx").innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020. We just do what inspires us and improve with every work. DEAD WORLD FOREVER.";
+}
