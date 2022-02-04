907896326a1124522c69c1ab5861fcbadb6e1fe5
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-04 14:17:48 +0700

    Delete browsercheck.js

--- browsercheck.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-
-        var text = document.getElementById("txtx");
-        text.innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020."
-        
-    }
-}
