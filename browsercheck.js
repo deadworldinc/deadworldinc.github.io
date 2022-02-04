609843b953716960c1c8385439f80d8c2fa2f2c8
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-04 13:57:50 +0700

    Update browsercheck.js

--- browsercheck.js
+++ browsercheck.js
@@ -1,7 +1,8 @@
 window.onload = function(){
-      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
-        var txt = document.getElementById("txtx");
-        txt.innerHTML = 'DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020. We just do what inspires us and improve with every work. DEAD WORLD FOREVER.';
+        var text = document.getElementById("txtx");
+        text.innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020."
+        
     }
 }
