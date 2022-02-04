7f739ad88a60ba592225fbd56e26b62b2f27d336
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-04 12:16:29 +0700

    Update browsercheck.js

--- browsercheck.js
+++ browsercheck.js
@@ -2,6 +2,6 @@ window.onload = function(){
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
         var txt = document.getElementById("txtx");
-        txt.innerHTML = 'DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020. We just do what inspires us and improve with every work. DEAD WORLD FOREVER.<br><br><img src="members/vk2.png" class="links"><a d="vk" class="hyper">https://vk.com/deadworldinc</a><br> <img src="members/inst2.png" class="links"><a id="instagram" class="hyper">https://instagram.com/deadworld.inc</a><br><img src="members/github.png" class="links"> <a id="github" class="hyper">deadworldinc.github.io</a>';
+        txt.innerHTML = 'DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020.<br><br><img src="members/vk2.png" class="links"><a d="vk" class="hyper">https://vk.com/deadworldinc</a><br> <img src="members/inst2.png" class="links"><a id="instagram" class="hyper">https://instagram.com/deadworld.inc</a><br><img src="members/github.png" class="links"> <a id="github" class="hyper">deadworldinc.github.io</a>';
     }
 }
