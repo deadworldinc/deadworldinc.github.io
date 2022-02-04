03a9eb0a595a67baaae1885f1fa104350adb5fa7
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-04 14:18:22 +0700

    Update links.js

--- links.js
+++ links.js
@@ -6,7 +6,7 @@ window.onload = function(){
     var text = document.getElementById("txtx");
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        text.innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020."
+        text.innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020. Our community consists of 8 members: SOULBURN, TXXXC, REVNACK, CLOROXCIDE, GOD OF BLOOD, PRISTUP, EMOWHIP and RAWSTAR. We just do what inspires us and improve with every work. DEAD WORLD FOREVER."
     }
     
     else{
