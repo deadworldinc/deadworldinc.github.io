dd338e6e36d423816414a87b76746bb8d6290559
deadworldinc <98756973+deadworldinc@users.noreply.github.com>
2022-02-04 14:14:31 +0700

    Update links.js

--- links.js
+++ links.js
@@ -3,17 +3,24 @@ window.onload = function(){
     var instagram = document.getElementById("instagram");
     var github = document.getElementById("github");
     var gmail = document.getElementById("gmail");
+    var text = document.getElementById("txtx");
 
-    vk.onclick = function(){
-        open("https://vk.com/deadworldinc");
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        text.innerHTML = "DEAD WORLD is an international creative community of designers, beatmakers and performers founded in 2020."
     }
-    instagram.onclick = function(){
-        open("https://www.instagram.com/deadworld.inc");
-    }
-    github.onclick = function(){
-        open("https://github.com/deadworldinc/deadworldinc.github.io");
-    }
-    gmail.onclick = function(){
-        open("https://gmail.com/");
+    
+    else{
+        vk.onclick = function(){
+            open("https://vk.com/deadworldinc");
+        }
+        instagram.onclick = function(){
+            open("https://www.instagram.com/deadworld.inc");
+        }
+        github.onclick = function(){
+            open("https://github.com/deadworldinc/deadworldinc.github.io");
+        }
+        gmail.onclick = function(){
+            open("https://gmail.com/");
+        }
     }
 }
